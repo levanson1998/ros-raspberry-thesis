5fb653c8725c2cb02362561bed18961250c3731b
levanson1998 <vanson1243@gmail.com>
2020-09-26 15:18:45 +0700

    get data lidar

--- src/lidar_sensor/scripts/lidar_data.py
+++ src/lidar_sensor/scripts/lidar_data.py
@@ -2,9 +2,9 @@
 
 import rospy
 from std_msgs.msg import String
-from sensor_msgs.msg import LaserScan
+from sensor_msgs.msg import LaserScan, PointCloud2
 import math
-from rplidar import RPLidar
+# from rplidar import RPLidar
 
 
 def deg2rad(deg):
@@ -19,27 +19,33 @@ laser_frequency = 10
 
 count = 0
 r = rospy.Rate(5.0)
+
+scan = LaserScan()
+
+scan.serial_port("/dev/ttyUSB0")
+scan.serial_baudrate(115200)
+
+scan.header.stamp = current_time
+scan.header.frame_id = 'laser_frame'
+scan.angle_min = deg2rad(-90)
+scan.angle_max = deg2rad(90)
+scan.angle_increment = deg2rad(180) / num_readings
+scan.time_increment = (5.0 / laser_frequency) / (num_readings)
+scan.range_min = 0.0
+scan.range_max = 100.0
+
+scan.ranges = []
+scan.intensities = []
+
 while not rospy.is_shutdown():
     current_time = rospy.Time.now()
 
-    scan = LaserScan()
 
-    scan.header.stamp = current_time
-    scan.header.frame_id = 'laser_frame'
-    scan.angle_min = deg2rad(-90)
-    scan.angle_max = deg2rad(90)
-    scan.angle_increment = deg2rad(180) / num_readings
-    scan.time_increment = (5.0 / laser_frequency) / (num_readings)
-    scan.range_min = 0.0
-    scan.range_max = 100.0
-
-    scan.ranges = []
-    scan.intensities = []
-    for i in range(0, num_readings):
-        scan.ranges.append(1.0 * count)  # fake data
-        scan.intensities.append(1)  # fake data
-    rospy.loginfo(scan)
-    scan_pub.publish(scan)
-    count += 1
+    # for i in range(0, num_readings):
+    #     scan.ranges.append(1.0 * count)  # fake data
+    #     scan.intensities.append(1)  # fake data  
+    rospy.loginfo(scan.ranges)
+    scan_pub.publish(scan.ranges)
+    # count += 1
     r.sleep()
 
